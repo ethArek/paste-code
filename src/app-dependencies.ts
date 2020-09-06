7f714fee6c8582f47af40f5a2d2ad7f5a1862630
ethArek <arkadiusz.sidzinka@gmail.com>
2020-09-06 15:20:25 +0200

    feat: add snippet service to app dependencies

--- src/app-dependencies.ts
+++ src/app-dependencies.ts
@@ -1,11 +1,18 @@
 import { DbService } from "./modules/database/db-service";
+import { SnippetService } from "./modules/snippet/snippet-service";
+import { SnippetRepository } from "./modules/snippet/snippet-repository";
 
 export interface AppDependencies {
+  snippetService: SnippetService
 }
 
 export function initAppDependencies (): AppDependencies {
   const dbService = new DbService();
 
+  const snippetRepository = new SnippetRepository(dbService);
+  const snippetService = new SnippetService(snippetRepository);
+
   return {
+    snippetService
   }
 }
