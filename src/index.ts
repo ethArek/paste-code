2c318b38ee09ebfb33871e75931c8727ba189904
ethArek <arkadiusz.sidzinka@gmail.com>
2020-09-20 15:27:30 +0200

    feat: add create language route

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,18 @@ import express from 'express';
 import { initAppDependencies } from './app-dependencies';
 import { getSnippetsList, getSnippetById } from './controllers/snippet';
 import errorHandlingMiddleware from './lib/error-handling-middleware';
+import { createLanguage } from './controllers/language';
 
 const app = express();
 const PORT = 3000;
 const deps = initAppDependencies();
 
+app.use(express.json());
+
 app.get('/snippets', (req, res, next) => getSnippetsList(req, res, next, deps));
 app.get('/snippets/:id', (req, res, next) => getSnippetById(req, res, next, deps));
+
+app.post('/languages', (req, res, next) => createLanguage(req, res, next, deps));
 
 app.use(errorHandlingMiddleware);
 
